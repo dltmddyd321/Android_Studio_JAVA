970779a0e4b491d6eea46e9d21f9038a9d8478d9
dltmddyd321 <dltmddyd321@naver.com>
2021-08-02 18:41:22 +0900

    2021_08_02_PDF_Reader
    PDF_Reader
    
    1. 파일이 있어도 null 오류가 발생하는 문제점.

---
+++ PDF_Reader/app/src/main/java/com/example/pdf_reader/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.pdf_reader;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PdfAdapter pdfAdapter;
+    private List<File> pdfList;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runTimePermission();
+    }
+
+    private void runTimePermission() {
+        Dexter.withContext(MainActivity.this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        displayPdf();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(MainActivity.this,"권한 요청!",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    public ArrayList<File> findPdf (File file)
+    {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = file.listFiles();
+
+
+        for (File singleFile: files)
+        {
+            if(singleFile.isDirectory() && !singleFile.isHidden()){
+                arrayList.addAll(findPdf(singleFile));
+            }
+            else {
+                if(singleFile.getName().endsWith(".pdf")){
+                    arrayList.add(singleFile);
+                }
+            }
+        }
+        return arrayList;
+    }
+
+    private void displayPdf() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+        pdfList = new ArrayList<>();
+        pdfList.addAll(findPdf(Environment.getExternalStorageDirectory()));
+        pdfAdapter = new PdfAdapter(this,pdfList);
+        recyclerView.setAdapter(pdfAdapter);
+    }
+}
